43fed4e22eae28067929bd89ece655f948b4fdf4
Valdir08 <valdirluciano54@gmail.com>
2025-06-17 23:12:31 +0000

    aulapoo

---
+++ nasaapi/src/test/java/com/unipam/AppTest.java
@@ -0,0 +1,20 @@
+package com.unipam;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
